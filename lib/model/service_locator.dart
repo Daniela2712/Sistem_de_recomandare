5dec8a1962280362eaaba11eb69b4fd4b3103fd9
Daniela <j.daniela271@gmail.com>
2022-01-02 23:01:26 +0200

    generate database+CRUD

---
+++ lib/model/service_locator.dart
@@ -0,0 +1,11 @@
+
+import 'package:get_it/get_it.dart';
+import 'package:sistem_de_recomandare/model/database_manager.dart';
+
+import 'database_manager.dart';
+
+GetIt locator = GetIt.instance;
+
+Future setupLocator() async {
+locator.registerSingleton(AppDatabase(openConnection()));
+}
